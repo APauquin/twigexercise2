61a16eb13d8694e1fb84430a6e9d9c52af4b362e
APauquin <alexandrep123@yahoo.com>
2023-04-14 11:38:51 +0200

    solved exercise 3

--- public/index.php
+++ public/index.php
@@ -7,3 +7,7 @@ require_once __DIR__ . '/../config/twig.php';
 $name = 'Wilder';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+
+$author = 'Oscar Wilder';
+
+echo $twig->render('index.html.twig', ['author' => $author]);
